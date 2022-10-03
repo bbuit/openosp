8d0ab47a7ef90f5136258298787ac76ebaa833db
bbuit <114422962+bbuit@users.noreply.github.com>
2022-10-03 10:32:47 -0400

    Update setup-faxws.sh

--- bin/setup-faxws.sh
+++ bin/setup-faxws.sh
@@ -20,19 +20,19 @@ docker-compose up -d faxws
 
 sleep 3
 
-docker-compose exec db mysql -h localhost -uroot -p$MYSQL_ROOT_PASSWORD -e "drop database if exists OscarFax"
-docker-compose exec db mysql -h localhost -uroot -p$MYSQL_ROOT_PASSWORD -e "create database OscarFax"
+docker-compose exec db mysql -h localhost -uroot -p${MYSQL_ROOT_PASSWORD} -e "drop database if exists OscarFax"
+docker-compose exec db mysql -h localhost -uroot -p${MYSQL_ROOT_PASSWORD} -e "create database OscarFax"
 
 # Run migrations
 # [cvo] use sql bootstrap instead for now.
 #docker-compose exec faxws bash -c "cd /usr/local/tomcat/webapps/faxWs/META-INF/maven/oscarFax/FaxWs && mvn flyway:migrate -Dflyway.user=root -Dflyway.password=tzcbU/u87wM= -Dflyway.url=jdbc:mysql://db/oscarFax"
 docker-compose cp faxws:/create_database.sql _bootstrap.sql
 
-docker-compose exec db bash -c "mysql -h localhost -uroot -p$MYSQL_ROOT_PASSWORD OscarFax < _bootstrap.sql"
+docker-compose exec db bash -c "mysql -h localhost -uroot -p${MYSQL_ROOT_PASSWORD} OscarFax < _bootstrap.sql"
 rm _bootstrap.sql
 
 echo "Setting authentication database..."
-docker-compose exec db mysql -h localhost -uroot -p$MYSQL_ROOT_PASSWORD OscarFax -e "insert into users Values('$tomcatUser','$tomcatPassword')"
-docker-compose exec db mysql -h localhost -uroot -p$MYSQL_ROOT_PASSWORD OscarFax -e "insert into user_roles Values('$tomcatUser','user')"
+docker-compose exec db mysql -h localhost -uroot -p${MYSQL_ROOT_PASSWORD} OscarFax -e "insert into users Values('${tomcatUser}','${tomcatPassword}')"
+docker-compose exec db mysql -h localhost -uroot -p${MYSQL_ROOT_PASSWORD} OscarFax -e "insert into user_roles Values('${tomcatUser}','user')"
 
 
