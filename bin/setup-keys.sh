20bb402a7daaa8be7512d4bd06b724ce31869f4a
bbuit <114422962+bbuit@users.noreply.github.com>
2022-10-03 10:27:44 -0400

    Update setup-keys.sh

--- bin/setup-keys.sh
+++ bin/setup-keys.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f ./volumes/ssl.key ] || [ ! -f ./volumes/ssl.crt ] ; then 
     echo "Generating self-signed cert for internal use. The common name is 'faxws' since that is the only internally validated name."
-    openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=CA/ST=BC/L=Vancouver/O=OpenOSP/CN=faxws" -keyout ./volumes/ssl.key -out ./volumes/ssl.crt
+    openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=CA/ST=BC/L=Toronto/O=OpenOSP/CN=faxws" -keyout ./volumes/ssl.key -out ./volumes/ssl.crt
 fi
 
 # Create an empty chain.pem file
