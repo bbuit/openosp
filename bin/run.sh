4b96066d2db38da27e03ac225d1169cb09f32842
bbuit <114422962+bbuit@users.noreply.github.com>
2022-10-17 17:47:07 -0400

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Setting up database..."
-docker-compose up -d db
+docker compose up -d db
 sleep 10
 
 echo "Bringing up tomcat"
-docker-compose up -d oscar
+docker compose up -d oscar
 
 echo "expediting Expedius"
-docker-compose up -d expedius
+docker compose up -d expedius
 
 echo "OSCAR is set up at http://<this host>/oscar"
 
