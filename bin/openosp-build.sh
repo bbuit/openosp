146cefc9d68dd04796369d5ba07c004a5014023e
bbuit <114422962+bbuit@users.noreply.github.com>
2022-10-17 17:39:45 -0400

    Update openosp-build.sh

--- bin/openosp-build.sh
+++ bin/openosp-build.sh
@@ -15,13 +15,13 @@ case "${COMMAND}" in
         OSCAR_OUTPUT=docker/oscar
         if [[ "$*" == *--test* ]]
         then
-            docker-compose up -d db
+            docker compose up -d db
         fi
 
         if [ -z "$WARFILE" ]
         then
             echo "Compiling OSCAR. This may take some time...."
-            docker-compose -f docker-compose.build.yml run --rm builder ./bin/build-oscar.sh
+            docker compose -f docker compose.build.yml run --rm builder ./bin/build-oscar.sh
             mv $OSCAR_OUTPUT/oscar/target/oscar-*-SNAPSHOT.war $OSCAR_OUTPUT/oscar.war
         else
             mkdir -p $OSCAR_OUTPUT
@@ -45,17 +45,17 @@ case "${COMMAND}" in
           echo "Retrieving current DrugRef2 binary"
           docker run --rm -v $(pwd):/code/ alpine sh -c "apk add curl && cd /code/ && curl -Lo $OSCAR_OUTPUT/drugref2.war $DRUGREF_WAR"
         fi
-        docker-compose build oscar
+        docker compose build oscar
         ;;
     expedius)
-        docker-compose build expedius
+        docker compose build expedius
         ;;
     faxws)
         echo "Compiling FaxWS"
-        docker-compose -f docker-compose.build.yml run --rm builder ./bin/build-faxws.sh
+        docker compose -f docker compose.build.yml run --rm builder ./bin/build-faxws.sh
 
         echo "Building FaxWs Docker Image"
-        docker-compose build faxws
+        docker compose build faxws
         ;;
 esac
 
