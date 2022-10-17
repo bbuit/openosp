9e9a64a3e30274a77f87af292de67f27a567466f
bbuit <114422962+bbuit@users.noreply.github.com>
2022-10-17 17:53:08 -0400

    Update openosp-update.sh

--- bin/openosp-update.sh
+++ bin/openosp-update.sh
@@ -6,5 +6,5 @@ docker pull openosp/open-osp:release
 docker pull openosp/expedius:latest
 docker pull openosp/faxws:latest
 
-docker-compose --compatibility up -d
+docker compose --compatibility up -d
 
